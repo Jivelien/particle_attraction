84626e1d77fb6e63089094666f00c0586cfdfce3
Jivelien <julien.delannoy@hotmail.com>
2024-09-13 11:40:35 +0200

    velocity test named

--- tests/test_particle.py
+++ tests/test_particle.py
@@ -15,14 +15,14 @@ class TestPosition(unittest.TestCase):
 
 
 class TestVelocity(unittest.TestCase):
-    def test_aaa(self):
+    def test_velocities_can_be_added(self):
         v1 = Velocity(dx=1, dy=0)
         v2 = Velocity(dx=0, dy=1)
 
         sut = v1 + v2
         self.assertEqual(Velocity(1, 1), sut)
 
-    def test_bbb(self):
+    def test_velocity_can_be_multiply_per_a_factor(self):
         v1 = Velocity(dx=1, dy=1)
         sut = v1 * 2
 

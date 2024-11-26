e8f5029ea12b6658606ecc13d54358d6e89b9d35
Jivelien <julien.delannoy@hotmail.com>
2024-11-26 15:07:56 +0100

    Attraction law type hint

--- particle_attraction_lib/attraction_law.py
+++ particle_attraction_lib/attraction_law.py
@@ -4,16 +4,16 @@ from typing import Dict, Tuple
 
 class AttractionLawInterface(ABC):
     @abstractmethod
-    def between(self, a_species: int, another_species: int):
+    def between(self, a_species: int, another_species: int) -> float:
         raise NotImplementedError
 
 
 class AttractionLaw(AttractionLawInterface):
-    def __init__(self):
+    def __init__(self) -> None:
         self.laws: Dict[Tuple[int, int], float] = {}
 
     def between(self, a_species: int, another_species: int) -> float:
         return self.laws.get((a_species, another_species), 0)
 
-    def add(self, a_species: int, another_species: int, attraction: float):
+    def add(self, a_species: int, another_species: int, attraction: float) -> None:
         self.laws[a_species, another_species] = attraction
